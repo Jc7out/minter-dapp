bd962eba284d7f6ca67c7b6ce446c7a20ed6686a
Jc7out <jc7out@gmail.com>
2022-03-21 15:52:46 -0400

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0xF3FbF2056024a9497E17c36A7C915333a375605e"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
